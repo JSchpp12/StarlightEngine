3067716e7518cdc39f04ce09c2a87fdf7bb5ad1e
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-05-30 19:07:13 -0400

    Removal of extra submodules and cleanup

--- src/StarlightEngine.cpp
+++ src/StarlightEngine.cpp
@@ -1,9 +1,20 @@
 #include "StarlightEngine.h"
+#include "SC/Shader.h"
+#include "ShaderManager/ShaderManager.h"
 
-#include "ShaderManager.h"
+#include <memory>
+
+using namespace star;
 
 int main() {
-    int handler = star::shadermanager::ShaderManager::AddShader(""); 
+    // std::unique_ptr<shadermanager::ShaderManager> shaderManager(new shadermanager::ShaderManager()); 
+    std::shared_ptr<common::ConfigFile> configFile(new common::ConfigFile("../Engine.cfg")); 
+
+    shadermanager::ShaderManager shaderManager = shadermanager::ShaderManager(configFile); 
+
+    auto vertShaderPath = configFile->GetSetting(star::common::Config_Settings::mediadirectory) + "shaders/vertShader.vert"; 
+
+    shaderManager.AddShader(vertShaderPath); 
 
     std::cout << "YAY" << std::endl; 
 }
