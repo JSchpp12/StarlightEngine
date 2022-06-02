1daafdf11d42500277306d1939a6136600d7f83f
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-06-02 14:07:15 -0400

    Fix

--- src/ObjectApp.h
+++ src/ObjectApp.h
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include "SC/Application.hpp"
+#include "SC/ConfigFile.hpp"
 #include "ShaderManager.h"
 #include "ObjectManager.h"
 #include "TextureManager.h"
@@ -11,7 +12,7 @@
 namespace star{
     class ObjectApp : public common::Application{
     public: 
-        ObjectApp(const std::string& pathConfigFile); 
+        ObjectApp(common::ConfigFile* configFile, core::ShaderManager* shaderManager, core::ObjectManager* objectManager, core::TextureManager* textureManager); 
 
         virtual void Load(); 
 
--- src/StarlightEngine.cpp
+++ src/StarlightEngine.cpp
@@ -11,9 +11,13 @@ using namespace star;
 
 int main() {
     // std::unique_ptr<shadermanager::ShaderManager> shaderManager(new shadermanager::ShaderManager()); 
-    // std::shared_ptr<common::ConfigFile> configFile(new common::ConfigFile("../Engine.cfg")); 
+    
+    std::unique_ptr<common::ConfigFile> configFile(new common::ConfigFile("../Engine.cfg")); 
+    std::unique_ptr<star::core::ShaderManager> shaderManager(new star::core::ShaderManager());
+    std::unique_ptr<star::core::ObjectManager> objectManager(new star::core::ObjectManager());
+    std::unique_ptr<star::core::TextureManager> textureManager(new star::core::TextureManager()); 
 
-    auto application = star::ObjectApp("../Engine.cfg"); 
+    auto application = star::ObjectApp(configFile.get(), shaderManager.get(), objectManager.get(), textureManager.get()); 
     application.Load(); 
 
     std::cout << "YAY" << std::endl; 
