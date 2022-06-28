482d2461eadace9d2a327fd21adc860f2365819a
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-06-27 22:26:16 -0400

    Addition of scenebuilder(unused)

---
+++ src/SceneBuilder.cpp
@@ -0,0 +1,5 @@
+#include "WorldBuilder.hpp"
+
+namespace star {
+
+}
---
+++ src/SceneBuilder.hpp
@@ -0,0 +1,63 @@
+#pragma once 
+
+#include "SC/Light.hpp"
+
+
+#include <memory>
+
+namespace star {
+	class WorldBuilder {
+	public:
+		class GameObjects {
+		public:
+
+		private:
+
+
+		};
+
+		class Lights {
+		public:
+			class Builder {
+			public:
+				Builder& setType(); 
+				Builder& setColor(); 
+				Builder& setPosition(); 
+
+			private:
+
+
+			};
+		private:
+
+
+		};
+		class Builder {
+		public:
+			class Object {
+			public:
+
+			private:
+
+
+			};
+
+			class Light {
+			public:
+
+			private:
+
+			};
+		private:
+
+		};
+
+		WorldBuilder(); 
+		~WorldBuilder(); 
+
+
+	private:
+
+
+	};
+}
