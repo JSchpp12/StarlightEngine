51224ea8741ee4820e0e7e33356362f0fd7b1e2a
JSchpp12 <jacob.schaupp@gmail.com>
2022-06-30 19:00:54 -0400

    General Cleanup

--- src/CameraController.h
+++ src/CameraController.h
@@ -3,7 +3,7 @@
 #include "SC/Interactivity.hpp"
 #include "SC/Time.hpp"
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 #include <iostream>
--- src/InteractionSystem.h
+++ src/InteractionSystem.h
@@ -2,7 +2,7 @@
 
 #include "SC/Interactivity.hpp"
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 #include <optional>
 #include <functional>
