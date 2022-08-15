d1d80d7ee32a4d860c9f8ec12ae5c4cab51f1125
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-08-15 17:46:33 -0400

    Update Submodules

--- src/CameraController.cpp
+++ src/CameraController.cpp
@@ -62,8 +62,11 @@ void star::CameraController::mouseButtonCallback(int button, int action, int mod
 
 void star::CameraController::worldUpdate() {
 	//TODO: improve time var allocation 
+	if ((double)time.timeElapsedLastFrameSeconds() > 1) {
+		time.updateLastFrameTime();
+	}
 	if (this->moveLeft || this->moveRight || this->moveForward || this->moveBack) {
-		float moveAmt = 50.0f * time.timeElapsedLastFrameSeconds();
+		float moveAmt = 0.1f * time.timeElapsedLastFrameSeconds();
 
 		glm::vec3 cameraPos = this->getPosition();
 		glm::vec3 cameraLookDir = -this->getLookDirection();
